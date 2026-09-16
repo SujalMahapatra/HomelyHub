fb41807be54b378904952451c23738688bb77db5
Sujal Mahapatra <159683157+SujalMahapatra@users.noreply.github.com>
2026-09-16 12:50:26 +0530

    Add Property Model

---
+++ backend/src/Models/propertyModel.js
@@ -0,0 +1,124 @@
+import slugify from 'slugify';
+import mongoose from 'mongoose';
+
+const propertySchema = new mongoose.Schema({
+    propertyName: {
+        type: String,
+        required: [true, "Please enter your property name"]
+    },
+
+    description: {
+        type: String,
+        required: [true, "Please add information about your property"]
+    },
+
+    extraInfo: {
+        type: String,
+        deafualt:"checkin on time, good services."
+    },
+
+    propertyType: {
+        type: String,
+        enum: ["House","Flat","Guest House","Hotel"],
+        default:"House"
+    },
+
+    roomType: {
+        type: String,
+        enum: ["Anytype","Room","Entire Home"],
+        default:"Anytype"
+    },
+
+    maximumGuest: {
+        type: Number,
+        required: [true, "Please give the maximum no. of Guest that can occupy."]
+    },
+
+    amenities: [
+        {
+            name: {
+                type: String,
+                required: true,
+                enum: [
+                    "WiFi",
+                    "Kitchen",
+                    "AC",
+                    "Washing Machine",
+                    "T.V",
+                    "Pool",
+                    "Free Parking",
+                    "Refrigerator"
+                ]
+            },
+
+            icon: {
+                type: String,
+                required: true
+            }
+
+        }
+    ],
+
+    images: {
+        type: [
+            {
+                public_id:{
+                    type: String
+                },
+                url:{
+                    type: String,
+                    required: true
+                }
+
+            }
+        ],
+        validate:{
+            validator:function(arr){
+                return arr.length>=6;
+            },
+            message:"The images must contain at least 6 images"
+
+        }
+    },
+
+    price: {
+        type: Number,
+        required: [true, "Please enter the price per night value"],
+        default: 500
+    },
+
+    address: {
+        area: String,
+        city: String,
+        state: String,
+        pincode: Number
+    },
+
+    currentBookings: {
+
+    },
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+
+    slug: String,
+
+    checkInTime: {type:String, default:"11:00"},
+    checkOutTime: {type:String, default:"13:00"}
+})
+
+propertySchema.pre("save", function(next){
+    this.slug=slugify(this.propertyName,{lower:true});
+    next();
+})
+
+propertySchema.pre("save", function(next){
+    this.address.city=this.address.city.toLowerCase().replaceAll(" ","");
+    next();
+})
+
+const Property= mongoose.model("Property", propertySchema);
+
+export {Property};
